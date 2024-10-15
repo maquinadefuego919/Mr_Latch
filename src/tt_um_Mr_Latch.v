34cfff6108e34408c85a42b285f058c4f47da06b
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-14 20:09:09 -0700

    Update tt_um_Mr_Latch.v

--- src/tt_um_Mr_Latch.v
+++ src/tt_um_Mr_Latch.v
@@ -20,23 +20,14 @@ module tt_um_Mr_Latch (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-wire opamp_nonlinear(in_p,in_n, out_p,out_n,vcm_out);
-voltage in_p, in_n,out_p,out_n,vcm_out;
-parameter real Rin = 50 from (0:inf];
-parameter real Rout = 50 from (0:inf];
-parameter real Gp_dB = 10 from [-100:100];
-parameter real a2 = 0 from [0:inf];
-parameter real IP1dB =0 from [-100:100];
-real Gp=4*(10**(Gp_dB/10)); // The factor 4 is to account for the power loss in the input and output impedances.
-real a1=((Gp*Rout)/Rin)**0.5;
-real IP1dB_magnitude=(10**(IP1dB/10))*1e-3;
-real Vin1dBc=(2*Rin* IP1dB_magnitude)**0.5;
-real a3=(4*0.109*a1)/(3*(Vin1dBc**2));
-analog begin
-    V(out_p,vcm_out)<+ 0.5*(a1*V(in_p,in_n)+a2*(V(in_p,in_n)**2)-a3*(V(in_p,in_n)**3));
-    V(out_n,vcm_out)<+ -0.5*(a1*V(in_p,in_n)+a2*(V(in_p,in_n)**2)-a3*(V(in_p,in_n)**3));
-end
+wire In, Out;
 
+    supply1 VDPWR;
+    supply0 VGND;
+    
+    pmos (Out,VDPWR ,In);
+    nmos (Out,VGND,In);
+    
     
   assign uo_out[2] = VGND;
   assign uo_out[3] = VGND;
