0b2868db214acdaa5f2e5a89f2a5826af9c9aa83
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-14 19:56:07 -0700

    Update tt_um_Mr_Latch.v

--- src/tt_um_Mr_Latch.v
+++ src/tt_um_Mr_Latch.v
@@ -27,14 +27,40 @@ assign Out=ua[1] ;
 
     buf Buf1(Out,In);
 
-    assign ua[5:2] = 4'b0000;
+    //assign ua[5:2] = 4'b0000;
     //assign uo_out[7:1] = 7'b0000000; 
-    assign ui_in[7:0] = 8'b00000000;
+    //assign ui_in[7:0] = 8'b00000000;
 
+ // assign uo_out[0] = VGND;
+  //assign uo_out[1] = VGND;
+  assign uo_out[2] = VGND;
+  assign uo_out[3] = VGND;
+  assign uo_out[4] = VGND;
+  assign uo_out[5] = VGND;
+  assign uo_out[6] = VGND;
+  assign uo_out[7] = VGND;
 
+  assign uio_out[0] = VGND;
+  assign uio_out[1] = VGND;
+  assign uio_out[2] = VGND;
+  assign uio_out[3] = VGND;
+  assign uio_out[4] = VGND;
+  assign uio_out[5] = VGND;
+  assign uio_out[6] = VGND;
+  assign uio_out[7] = VGND;
 
-  assign uio_out = 0;
-   assign uio_oe  = 0;
+  assign uio_oe[0] = VGND;
+  assign uio_oe[1] = VGND;
+  assign uio_oe[2] = VGND;
+  assign uio_oe[3] = VGND;
+  assign uio_oe[4] = VGND;
+  assign uio_oe[5] = VGND;
+  assign uio_oe[6] = VGND;
+  assign uio_oe[7] = VGND;
+  
+
+  //assign uio_out = 0;
+   //assign uio_oe  = 0;
     //assign uio_in =0
   // List all unused inputs to prevent warnings
 wire _unused = &{ena, clk, rst_n, 1'b0};
